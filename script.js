8f021b928febc88b1ff486f664a6e9305df7322d
ankushgarg44 <146913479+ankushgarg44@users.noreply.github.com>
2025-01-02 23:17:20 +0530

    main

---
+++ script.js
@@ -0,0 +1,8 @@
+function goToSemester(semesterNumber) {
+    if (semesterNumber === 1) {
+        window.location.href = "Semester1/semester1.html";
+    } else if (semesterNumber === 2) {
+        window.location.href = "Semester2/semester2.html";
+    }
+    // You can add more semesters if needed.
+}
